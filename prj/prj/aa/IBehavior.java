e2ec16d1d78c1f81f97bdd50146f0f562ac5f4c8
Vectoria <135844815+Vectoria@users.noreply.github.com>
2024-01-28 13:20:17 +0000

    Delete prj/prj directory

--- prj/prj/aa/IBehavior.java
+++
@@ -1,9 +0,0 @@
-package prj.aa;
-
-import processing.core.PVector;
-
-public interface IBehavior {
-    public PVector getDesiredVelocity(Boid me);
-    public void setWeight(float weight);
-    public float getWeight();
-}
--- prj/prj/ecosytem/IAnimal.java
+++
@@ -1,8 +0,0 @@
-package prj.ecosytem;
-
-public interface IAnimal {
-    public Animal reproduce(boolean mutate);
-    public void eat(Terrain terrain);
-    public void energy_consumption(float dt, Terrain terrain);
-    public boolean die();
-}
